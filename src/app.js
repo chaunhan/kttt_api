07b097cecd7ed926df898051b9d00fb2d80c7163
chaunhan <g.huy.ngo.bmt@gmail.com>
2024-03-20 18:05:06 +0700

    add file

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const apiRoute = require('./routes');
+require('dotenv').config()
+require('./models/mongo-provider')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// middleware application routes
+
+app.use((req, res, next) => {
+  // console.log('Log system:', process.env)
+  next()
+})
+
+app.get('/', function (req, res) {
+    res.send('Api is running')
+  })
+  
+app.use('/api', apiRoute)
+
+app.get('/api*', function (req, res) {
+    return res.json({'message': 'api not found'})
+  })
+
+
+app.listen(3000);
