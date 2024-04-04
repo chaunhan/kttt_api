0aad26bfaca459a4b467b16b4b7e2194c7bd28cd
chaunhan <g.huy.ngo.bmt@gmail.com>
2024-04-05 04:05:07 +0700

    123

--- API/README.md
+++
@@ -1,1 +0,0 @@
-# kttt_api
--- API/src/models/cart.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const cart = new mongoose.Schema({
-    id : {type : String},
-    TenCourse : {type: String},
-    GiaCourse : {type: Number},
-})
-
-module.exports = mongoose.model('cart', cart)
